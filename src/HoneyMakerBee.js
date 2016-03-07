1545785f1fbe1c6cad99860e7f594d903c085e1d
Danny Tunon <danieltunon@gmail.com>
2016-03-07 11:45:45 -0800

    Implement HoneyMakerBee sub-class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-var HoneyMakerBee = function() {
+/*global Bee*/
+
+var HoneyMakerBee = function HoneyMakerBee() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function makeHoney() {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function makeHoney() {
+  this.honeyPot--;
+};
